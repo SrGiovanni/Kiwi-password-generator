f4359194cc596694cdea7b661847e8c779b06d43
SrGiovanni <samuelefulghi@gmail.com>
2022-01-19 12:13:40 -0500

    add user input prompts

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,21 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
+  var passLength = ~~prompt("Please input a number equal to or greater than 8, and less than or equal to 128.");
+
+  var validPass = false;
+  passLength>=8 && passLength <=128 ? validPass = true : validPass = false;
+
+  while(!validPass){
+    passLength = ~~prompt("Invalid number entered. Please input a number equal to or greater than 8, and less than or equal to 128.");
+
+  }
+
+  var useUppercase = confirm("Use Uppercase characters?");
+  var useLowercase = confirm("Use Lowercase characters?");
+  var useNum = confirm("Use Numbers?");
+  var useSpecChar = confirm("Use Special Characters?");
+
   passwordText.value = password;
 
 }
