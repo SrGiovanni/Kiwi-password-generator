52ea903cd089268bc29c2d252ee9968c00fd176f
SrGiovanni <samuelefulghi@gmail.com>
2022-01-22 00:32:34 -0500

    chang implementation of character bank

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-const upperChars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-const lowerChars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-const numChars = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerChars = "abcdefghijklmnopqrstuvwxyz";
+const numChars = "0123456789";
 const specChars =  "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
 
 // Write password to the #password input
